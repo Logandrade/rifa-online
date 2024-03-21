557863cd963c1241214ed9e4930b64efe9aca696
Logandrade <123709407+Logandrade@users.noreply.github.com>
2024-03-21 14:52:11 -0300

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-const config = require('../knexfile')
-const knex = require('knex')(config)
-//nao usar isso em producao, por que quando iniciado o index
-knex.migrate.latest([config])
-module.exports = knex
